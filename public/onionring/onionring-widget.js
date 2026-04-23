3d4f781506132efca50114782911dfc6a7f1ff75
JAMIE or MOB ᶻ 𝘇 𐰁 <afudo581@gmail.com>
2026-04-23 18:55:28 -0500

    ???

--- public/onionring/onionring-widget.js
+++ public/onionring/onionring-widget.js
@@ -61,7 +61,7 @@ else {
       <td class='webring-prev'><a href='${sites[previousIndex]}'>←</a></td>
       <td class='webring-info'><img src="https://i.postimg.cc/N0fxRXkF/korn-(4).png"></br>
       <span class='webring-links'>
-        ${indexText}
+        <a href='${indexPage}'>index</a>
       </span>
       <td class='webring-next'><a href='${sites[nextIndex]}'>→</a></td>
     </tr>
